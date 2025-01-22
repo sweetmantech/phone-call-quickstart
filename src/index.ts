0525ea68244e7ebf2296d97dce476524c3915194
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-22 18:03:47 -0300

    elevenlabsdemo

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,86 @@
-import dotenv from "dotenv";
-import twilio from "twilio";
-import VoiceResponse = require("twilio/lib/twiml/VoiceResponse");
+// src/app.ts
 
-dotenv.config();
+import "dotenv/config";
+import express, { Response } from "express";
+import ExpressWs from "express-ws";
+import VoiceResponse from "twilio/lib/twiml/VoiceResponse";
+import { ElevenLabsClient } from "elevenlabs";
+import { type WebSocket } from "ws";
+import { Readable } from "stream";
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const app = ExpressWs(express()).app;
+const PORT: number = parseInt(process.env.PORT || "5000");
 
-if (!accountSid || !authToken) {
-  throw new Error("Missing required environment variables");
-}
+const elevenlabs = new ElevenLabsClient();
+const voiceId = "21m00Tcm4TlvDq8ikWAM";
+const outputFormat = "ulaw_8000";
+const text = "This is a test. You can now hang up. Thank you.";
 
-const client = twilio(accountSid, authToken);
+function startApp() {
+  app.post("/call/incoming", (_, res: Response) => {
+    const twiml = new VoiceResponse();
 
-const twiml = new VoiceResponse();
-twiml.say(
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
-);
-const twimlString = twiml.toString();
-
-async function makeOutboundCall(): Promise<void> {
-  try {
-    const call = await client.calls.create({
-      twiml: twimlString,
-      to: process.env.TWILIO_TO_PHONE_NUMBER as string,
-      from: process.env.TWILIO_PHONE_NUMBER as string,
+    twiml.connect().stream({
+      url: `wss://${process.env.SERVER_DOMAIN}/call/connection`,
     });
 
-    console.log("Call successfully placed!");
-    console.log("Call SID:", call.sid);
-  } catch (error) {
-    console.error("Error making the call:", error);
-  }
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.end(twiml.toString());
+  });
+
+  app.ws("/call/connection", (ws: WebSocket) => {
+    ws.on("message", async (data: string) => {
+      const message: {
+        event: string;
+        start?: { streamSid: string; callSid: string };
+      } = JSON.parse(data);
+
+      if (message.event === "start" && message.start) {
+        const streamSid = message.start.streamSid;
+        const response = await elevenlabs.textToSpeech.convert(voiceId, {
+          model_id: "eleven_flash_v2_5",
+          output_format: outputFormat,
+          text,
+        });
+
+        const readableStream = Readable.from(response);
+        const audioArrayBuffer = await streamToArrayBuffer(readableStream);
+
+        ws.send(
+          JSON.stringify({
+            streamSid,
+            event: "media",
+            media: {
+              payload: Buffer.from(audioArrayBuffer as any).toString("base64"),
+            },
+          })
+        );
+      }
+    });
+
+    ws.on("error", console.error);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Local: http://localhost:${PORT}`);
+    console.log(`Remote: https://${process.env.SERVER_DOMAIN}`);
+  });
 }
 
-// Make the call
-makeOutboundCall();
+function streamToArrayBuffer(readableStream: Readable) {
+  return new Promise((resolve, reject) => {
+    const chunks: Buffer[] = [];
+
+    readableStream.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    readableStream.on("end", () => {
+      resolve(Buffer.concat(chunks).buffer);
+    });
+
+    readableStream.on("error", reject);
+  });
+}
+
+startApp();
