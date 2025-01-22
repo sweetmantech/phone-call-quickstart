0563ee018059bb442bd491d23574e21d6913b704
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-22 17:41:44 -0300

    make the text super long lorem ipsum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import twilio from "twilio";
+import VoiceResponse = require("twilio/lib/twiml/VoiceResponse");
 
 dotenv.config();
 
@@ -12,10 +13,16 @@ if (!accountSid || !authToken) {
 
 const client = twilio(accountSid, authToken);
 
+const twiml = new VoiceResponse();
+twiml.say(
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+);
+const twimlString = twiml.toString();
+
 async function makeOutboundCall(): Promise<void> {
   try {
     const call = await client.calls.create({
-      url: "http://demo.twilio.com/docs/voice.xml", // Replace with your TwiML URL
+      twiml: twimlString,
       to: process.env.TWILIO_TO_PHONE_NUMBER as string,
       from: process.env.TWILIO_PHONE_NUMBER as string,
     });
