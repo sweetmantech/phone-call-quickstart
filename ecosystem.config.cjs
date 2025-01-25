c8b0c154be4633c53df7591a86d9dc45f750f9cf
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-25 11:45:47 -0300

    fixing missing ts-node errors.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,15 @@ module.exports = {
   apps: [
     {
       name: "recoup-phone-call",
-      script: "pnpm",
+      script: "npm",
       args: "start",
       cron_restart: "0 */12 * * *",
       watch: false,
       autorestart: true,
+      env: {
+        PATH: "/root/.nvm/versions/node/v20/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        NODE_ENV: "production",
+      },
     },
   ],
 };
