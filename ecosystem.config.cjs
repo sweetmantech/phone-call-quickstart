986c0d0ee9395bc381b5c0168c32b7a9dc1db484
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-25 11:54:49 -0300

    update start script to use dev instead of run

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "recoup-phone-call",
       script: "npm",
-      args: "start",
+      args: "dev",
       cron_restart: "0 */12 * * *",
       watch: false,
       autorestart: true,
