6ba5524e2cdb19f0c413e8a268be60f5170f8c01
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-25 13:03:14 -0300

    second attempt pnpm dev

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,16 @@ module.exports = {
   apps: [
     {
       name: "recoup-phone-call",
-      script: "/usr/local/bin/pnpm",
+      script: "/root/.local/share/pnpm/pnpm",
       args: "dev",
       cron_restart: "0 */12 * * *",
       watch: false,
       autorestart: true,
       env: {
-        PATH: "/root/.nvm/versions/node/v20/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        PATH: "/root/.nvm/versions/node/v20/bin:/root/.local/share/pnpm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
         NODE_ENV: "production",
         NVM_DIR: "/root/.nvm",
+        PNPM_HOME: "/root/.local/share/pnpm",
       },
       interpreter: "/root/.nvm/versions/node/v20/bin/node",
     },
