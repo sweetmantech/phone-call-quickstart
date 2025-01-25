2e3c774feb058923688049f74bf998b9341b40c5
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-25 13:01:15 -0300

    fixing ecosystem start.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "recoup-phone-call",
-      script: "pnpm",
+      script: "/usr/local/bin/pnpm",
       args: "dev",
       cron_restart: "0 */12 * * *",
       watch: false,
@@ -10,7 +10,9 @@ module.exports = {
       env: {
         PATH: "/root/.nvm/versions/node/v20/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
         NODE_ENV: "production",
+        NVM_DIR: "/root/.nvm",
       },
+      interpreter: "/root/.nvm/versions/node/v20/bin/node",
     },
   ],
 };
