03ee655bc43ffdc72393c73dc65a317796150716
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-25 13:06:12 -0300

    after giving cursor more context.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,18 +2,17 @@ module.exports = {
   apps: [
     {
       name: "recoup-phone-call",
-      script: "/root/.local/share/pnpm/pnpm",
+      script: "/root/.nvm/versions/node/v20.18.2/bin/pnpm",
       args: "dev",
       cron_restart: "0 */12 * * *",
       watch: false,
       autorestart: true,
       env: {
-        PATH: "/root/.nvm/versions/node/v20/bin:/root/.local/share/pnpm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        PATH: "/root/.nvm/versions/node/v20.18.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
         NODE_ENV: "production",
         NVM_DIR: "/root/.nvm",
-        PNPM_HOME: "/root/.local/share/pnpm",
       },
-      interpreter: "/root/.nvm/versions/node/v20/bin/node",
+      interpreter: "/root/.nvm/versions/node/v20.18.2/bin/node",
     },
   ],
 };
